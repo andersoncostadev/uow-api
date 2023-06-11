525be3bff24096a2237443c0438efc4ec3199910
Anderson <brhdn313401@ambev.com.br>
2023-06-11 11:23:44 -0300

    feat: implemetation class to extension

---
+++ Cooperchip.DiretoaoPonto.UoW.Api/Configuration/Extensions/DataExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Cooperchio.DiretoAoPonto.Uow.Data.Orm;
+using Cooperchip.DiretoaoPonto.UoW.Api.Configuration.Settings;
+using Cooperchip.DiretoAoPonto.Uow.Data.FailedRepository;
+using Cooperchip.DiretoAoPonto.Uow.Data.Repositories.Abstraction;
+using Cooperchip.DiretoAoPonto.Uow.Data.Repositories.Implemetation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+namespace Cooperchip.DiretoaoPonto.UoW.Api.Configuration.Extensions
+{
+    public static class DataExtensions
+    {
+        public static IServiceCollection AddDbContextConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = configuration["MySQlConnection:MySQlConnectionString"];
+            services.AddDbContext<UoWDbContext>(options => options.
+                           UseMySql(connection,new MySqlServerVersion(new Version(8, 0, 5))));
+            services.AddControllers();
+
+            return services;
+        }
+
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cooperchip.DiretoaoPonto.UoW.Api - API", Version = "v1" });
+            });
+       
+            return services;
+        }
+
+        public static IServiceCollection AddDIRepositoryConfiguration(this IServiceCollection services)
+        {
+            services.AddScoped<IPassengerFailedRepository, PassengerFailedRepository>();
+            services.AddScoped<IFlightFailedRepository, FlightFailedRepository>();
+
+            services.AddScoped<IPassagenrRepository, PassengerRepository>();
+            services.AddScoped<IFlightRepository, FlightRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAppSettingsConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<FlightSettings>(configuration.GetSection(FlightSettings.SessionName));
+            services.AddSingleton(s => s.GetRequiredService<IOptions<FlightSettings>>().Value);
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Unit Of Work, Uow - API");
+            });
+
+            return app;
+        }
+    }
+}
--- Cooperchip.DiretoaoPonto.UoW.Api/Startup.cs
+++ Cooperchip.DiretoaoPonto.UoW.Api/Startup.cs
@@ -1,18 +1,11 @@
-﻿using Cooperchio.DiretoAoPonto.Uow.Data.Orm;
-using Cooperchip.DiretoaoPonto.UoW.Api.Configuration.Settings;
+﻿using Cooperchip.DiretoaoPonto.UoW.Api.Configuration.Extensions;
 using Cooperchip.DiretoaoPonto.UoW.Api.Mapper;
-using Cooperchip.DiretoAoPonto.Uow.Data.FailedRepository;
-using Cooperchip.DiretoAoPonto.Uow.Data.Repositories.Abstraction;
-using Cooperchip.DiretoAoPonto.Uow.Data.Repositories.Implemetation;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
 
 namespace Cooperchip.DiretoaoPonto.UoW.Api
 {
     public class Startup
     {
-        public IConfiguration Configuration { get;}
+        public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
         {
@@ -23,44 +16,24 @@ namespace Cooperchip.DiretoaoPonto.UoW.Api
         {
             services.AddAutoMapper(typeof(AutoMapperConfig));
 
-            services.AddScoped<IPassengerFailedRepository, PassengerFailedRepository>();
-            services.AddScoped<IFlightFailedRepository, FlightFailedRepository>();
-
-            services.AddScoped<IPassagenrRepository, PassengerRepository>();
-            services.AddScoped<IFlightRepository, FlightRepository>();
-
-            var connection = Configuration["MySQlConnection:MySQlConnectionString"];
-
-            services.AddDbContext<UoWDbContext>(options => options.
-                UseMySql(connection,
-                        new MySqlServerVersion(
-                            new Version(8, 0, 5))));
+            services.AddDIRepositoryConfiguration();
+            services.AddDbContextConfiguration(Configuration);
+            services.AddSwaggerConfiguration();
+            services.AddAppSettingsConfiguration(Configuration);
 
             services.AddControllers();
-
-                services.AddSwaggerGen(c =>
-                {
-                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cooperchip.DiretoaoPonto.UoW.Api - API", Version = "v1" });
-                });
-
-            services.Configure<FlightSettings>(Configuration.GetSection(FlightSettings.SessionName));
-            services.AddSingleton(s => s.GetRequiredService<IOptions<FlightSettings>>().Value);
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI( c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Unit Of Work, Uow - API");
-                });
+                app.UseSwaggerConfiguration();
             }
 
 
-            app.UseHttpsRedirection();   
+            app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthentication();
             app.UseEndpoints(endpoints =>
